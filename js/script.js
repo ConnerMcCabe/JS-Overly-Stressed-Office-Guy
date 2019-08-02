da43ac7943339b72eaa7de49ef7829c7d3787515
Admin <Admin@MacBook-Pro-2.local>
2019-08-02 15:24:04 -0500

    stuff is set up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 alert("mello shmello");
 
-const h1 = document
+// const h1 = document
+let a = 'cool'
+let b = 'party'
+let c = 'nvm'
+function name (d, e, f) {
+    a, b, c
+
+}
+//function expression
+const nameTwo = function () {
+
+}
+// arrow
+const nameThree = () => 'hello'
