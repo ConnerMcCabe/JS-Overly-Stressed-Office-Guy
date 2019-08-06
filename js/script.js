2decc45c67426640bc2090ece6f38d2f8f391d76
Admin <Admin@MacBook-Pro-2.local>
2019-08-06 12:38:53 -0500

    shuffler for the pictures added

--- js/script.js
+++ js/script.js
@@ -11,11 +11,13 @@ const PICTURES = {
     "6": "pic",
     "7": "pic",
     "8": "pic",
+    //etc 15
 };
+let numArray = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 /*----- app's state (variables) -----*/ 
 let grid, winner;
-
+let shuffler;
 /*----- cached element references -----*/ 
 
 
@@ -25,7 +27,7 @@ let grid, winner;
 // document.onkeydown
 document.addEventListener('keydown', logKey);
 
-function logKey(ev) {
+function logKey() {
   console.log(event);
 }
 
@@ -34,18 +36,34 @@ init();
 function init() {
 
     grid = [
-        [1, 0, 0, 0],
+        [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0]
     ];
-    
+
     // console.table(grid) too see the grid in console to check if values add or slide
     // logic behind moving just the blank tile
 }
-function shuffler() {
-// shuffle pictures on array
+//sanity check 2, color tiles for moving or shuffling
+// 
+// grid.forEach(function(colIdx, rowIdx) {
+// let div = document.getElementById(`c${colIdx}r${rowIdx}`);
+// div.style.backgroundColor = PICTURES[cell];
+// });
+// function shuffler() {
+//     PICTURES.sort(function () {
+//         return .5 - Math.random();
+//     })
+
+for(let x= 0; x<16;x++)
+{
+    shuffler = (Math.floor(Math.random() * 100))%numArray.length;
+    console.log(numArray.splice(shuffler,1));
 }
+
+// shuffle pictures on array
+
 function slide(ev) { 
   
 }
